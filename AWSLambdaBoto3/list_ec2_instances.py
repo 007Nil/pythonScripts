c73ad063fbba31f5df8994432dc05eb42de9ca50
007Nil <sagniksarkar@ymail.com>
2020-06-23 02:03:32 +0530

    list of ec2 instance in AWS

---
+++ AWSLambdaBoto3/list_ec2_instances.py
@@ -0,0 +1,26 @@
+import boto3
+
+
+# using resource
+'''
+ec2_resource = boto3.resource(service_name="ec2",region_name="ap-south-1")
+
+for each_instance in ec2_resource.instances.all():
+    print(each_instance.id, each_instance.state['Name'])
+'''
+# Declare variables for search instances
+
+instance_name = "C01"
+count =0
+
+ec2_client = boto3.client(service_name="ec2",region_name="ap-south-1")
+ec2_instance_details = ec2_client.describe_instances()
+# print(ec2_instance_details)
+for each_instance in ec2_instance_details['Reservations']:
+    # print(each_instance)
+    for each_instance_requried_details in each_instance['Instances']:
+        # print(each_instance_requried_details)
+        if instance_name == each_instance_requried_details["KeyName"]:
+            count +=1
+
+print(count)
