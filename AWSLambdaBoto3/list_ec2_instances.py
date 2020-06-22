511a12817c99c5f06fb81c68aaddd5b05bcee90b
007Nil <sagniksarkar@ymail.com>
2020-06-23 02:34:13 +0530

    Don't count terminated instances

--- AWSLambdaBoto3/list_ec2_instances.py
+++ AWSLambdaBoto3/list_ec2_instances.py
@@ -1,6 +1,5 @@
 import boto3
 
-
 # using resource
 '''
 ec2_resource = boto3.resource(service_name="ec2",region_name="ap-south-1")
@@ -10,17 +9,18 @@ for each_instance in ec2_resource.instances.all():
 '''
 # Declare variables for search instances
 
-instance_name = "C01"
-count =0
+instance_name = "test"
+count = 0
+terminated = "terminated"
 
-ec2_client = boto3.client(service_name="ec2",region_name="ap-south-1")
+ec2_client = boto3.client(service_name="ec2", region_name="ap-south-1")
 ec2_instance_details = ec2_client.describe_instances()
 # print(ec2_instance_details)
 for each_instance in ec2_instance_details['Reservations']:
     # print(each_instance)
     for each_instance_requried_details in each_instance['Instances']:
-        # print(each_instance_requried_details)
-        if instance_name == each_instance_requried_details["KeyName"]:
-            count +=1
+        print(each_instance_requried_details)
+        if instance_name == each_instance_requried_details["KeyName"] and each_instance_requried_details["State"]["Name"] != terminated:
+            count += 1
 
 print(count)
