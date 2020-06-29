58b4fde86d1ee8fac6caffc23b9366c2adbad39c
007Nil <sagniksarkar@ymail.com>
2020-06-30 03:36:10 +0530

    compute EC2 'UP TIME' display it using email(AWS SES)

--- AWSLambdaBoto3/list_ec2_instances.py
+++ AWSLambdaBoto3/list_ec2_instances.py
@@ -3,6 +3,8 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 from pprint import pprint
+from datetime import datetime, timezone
+import time
 import datetime
 
 # Declare variables for search instances
@@ -17,7 +19,9 @@ node_details = []
 
 #  Class Definition
 class Ec2InstanceDetails:
-    def __init__(self, instance_id=None, tags=None, state=None, launchDate=None, launchTime=None):
+    def __init__(self, instance_id=None, tags=None, state=None, launchDate=None, launchTime=None,
+                 runtimeEC2instance=None):
+        self.runtimeEC2instance = runtimeEC2instance
         self.launchTime = launchTime
         self.launchDate = launchDate
         self.state = state
@@ -26,7 +30,8 @@ class Ec2InstanceDetails:
 
 
 class NodeDetails:
-    def __init__(self, nodename=None, node_state=None, launchDate=None, launchTime=None):
+    def __init__(self, nodename=None, node_state=None, launchDate=None, launchTime=None, runtimeEC2instance=None):
+        self.runtimeEC2instance = runtimeEC2instance
         self.launchTime = launchTime
         self.launchDate = launchDate
         self.nodename = nodename
@@ -34,22 +39,28 @@ class NodeDetails:
 
     def dict(self):
         dict = {"Node Name": self.nodename, "Node State": self.node_state, "Launch Time": self.launchTime,
-                "Launch Date": self.launchDate}
+                "Launch Date": self.launchDate,"Up Time":self.runtimeEC2instance}
         return dict
 
 
 # ses configurations
 def ses_function(info_data):
-    sender = "Sagnik Sarkar<sagniksarkar@ymail.com>"
-    recipient = "sagnik.sarkar@simpsoftsolutions.com"
+    sender = ""
+    recipient = ""
     aws_region = "ap-south-1"
     table_details = ""
     for info_data_each_node_details in info_data['Node details']:
         table_details += "<tr>" \
                          "<td align='center'>" + info_data_each_node_details['Node Name'] + "</td>" \
-                         "<td align='center'>"+info_data_each_node_details['Node State'] + "</td>" \
-                         "<td align='center'>" + info_data_each_node_details['Launch Date'] +\
-                         " "+info_data_each_node_details["Launch Time"]+ "</td>" \
+                         "<td align='center'>" + \
+                         info_data_each_node_details['Node State'] +\
+                         "</td>" \
+                         "<td align='center'>" + \
+                         info_data_each_node_details['Launch Date'] + \
+                         " " + \
+                         info_data_each_node_details["Launch Time"] + "</td>" \
+                         "<td align='center'>" + \
+                         info_data_each_node_details["Up Time"] + "</td>" \
                          "</tr>"
 
     # print(table_details)
@@ -57,16 +68,28 @@ def ses_function(info_data):
 
     body_text = "This email was sent with Amazon SES using the AWS SDK for Python (Boto3)."
 
-    body_html = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' " \
-                "content='width=device-width, initial-scale=1.0'><title>Document</title></head><body><p " \
-                "align='center'>Cluster Name: " + info_data["Cluster Name"] + "</p><p align='center'>Node Count: " + \
-                info_data["Node Count"] + "</p><p align='center'>Node " \
-                                          "details</p><table style='width:50%' align='center'><tr><th>Node " \
-                                          "Name</th><th>Node " \
-                                          "State</th><th>Node Start Date and Time</th></tr>" + table_details + \
-                "</table></body></html>"
-
-    # print(body_html)
+    body_html = "<!DOCTYPE html>" \
+                "<html lang='en'>" \
+                "<head>" \
+                "<meta charset='UTF-8'>" \
+                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" \
+                "<title>Document</title>" \
+                "</head>" \
+                "<body>" \
+                "<p align='center'>Cluster Name: " + info_data["Cluster Name"] + "</p>" \
+                "<p align='center'>Node Count: " + info_data["Node Count"] + "</p>" \
+                "<p align='center'>Node details</p>" \
+                "<table style='width:50%' align='center'>" \
+                "<tr>" \
+                "<th>Node Name</th>" \
+                "<th>Node State</th>" \
+                "<th>Node Start Date and Time</th>" \
+                "<th>Up Time</th>" \
+                "</tr>" + table_details + \
+                "</table>" \
+                "</body>" \
+                "</html>"
+    print(body_html)
     CHARSET = "UTF-8"
 
     # Create a new SES resource and specify a region.
@@ -134,13 +157,21 @@ for each_instance in ec2_instance_details['Reservations']:
 
     # print(each_instance)
     for each_instance_requried_details in each_instance['Instances']:
-        launchTime = each_instance_requried_details["LaunchTime"]
-        # print(launchTime)
+        launchTime_utc = each_instance_requried_details["LaunchTime"]
+        now_utc = launchTime_utc.replace(tzinfo=timezone.utc)
+        now_local = now_utc.astimezone()
+        run_time_EC2_instance = "--"
+        # end = datetime.now()
+        if each_instance_requried_details["State"]["Name"] != "terminated":
+            end_time = datetime.datetime.now().replace(microsecond=0)
+            print(end_time)
+            run_time_EC2_instance= (end_time - now_local.replace(tzinfo=None))
         instance_required_details.append(Ec2InstanceDetails(each_instance_requried_details["InstanceId"],
                                                             each_instance_requried_details["Tags"],
                                                             each_instance_requried_details["State"]["Name"],
-                                                            str(launchTime.date()),
-                                                            str(launchTime.time())))
+                                                            str(now_local.date()),
+                                                            str(now_local.time()),
+                                                            str(run_time_EC2_instance)))
 
 # print(instance_requried_details[0].tags[0]["Key"])
 # print(instance_requried_details[0].state,'Cluster Name='+instance_requried_details[0].tags[0]["Value"]
@@ -171,18 +202,20 @@ if count_node > 0:
             if each_instance_list_data_in_tags["Value"] == cluster_name:
 
                 for tag_name in each_instance_list_data.tags:
+                    # print()
                     if tag_name["Key"] == "Name":
                         node_details.append(NodeDetails(tag_name["Value"], each_instance_list_data.state,
                                                         each_instance_list_data.launchDate,
-                                                        each_instance_list_data.launchTime).dict())
+                                                        each_instance_list_data.launchTime,
+                                                        each_instance_list_data.runtimeEC2instance).dict())
 
     publish_object = {"Cluster Name": cluster_name, "Node Count": str(count_node),
                       "Node details": node_details}
 
-    print(publish_object)
+    # print(publish_object)
     json_data = json.dumps(publish_object)
     info = json.loads(json_data)
-    # pprint(info)
+    pprint(info)
     ses_function(info)
     # pprint(info['Node details'][1]['Node Name'])
     # for each_node_details in info['Node details']:
@@ -193,3 +226,4 @@ else:
     print("Cluster " + cluster_name + " contains 0 nodes, Since it does not exist")
     publish_object = {"Cluster Name": cluster_name, "Node Count": str(count_node), "Node details": node_details}
     print(json.dumps(publish_object))
+
