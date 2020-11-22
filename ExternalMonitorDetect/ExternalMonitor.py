cb23965e3640d70617f8fa4c47b234079b423cd9
Nil <nil@lenovo-2957.007nil.com>
2020-11-22 23:52:21 +0530

    Add infinite loop

--- ExternalMonitorDetect/ExternalMonitor.py
+++ ExternalMonitorDetect/ExternalMonitor.py
@@ -2,30 +2,33 @@
 
 from subprocess import check_output
 import os
+import time
 
 
 command = "xrandr"
 
 requried_vga_string = "VGA1 connected"
-
-output = check_output([command, "-q"])
+while True:
+    output = check_output([command, "-q"])
 
 #print(output)
 
-# Check if VGA cable is connected or not
-# if connected then make VGA as primary and turn off laptop monitor
-if requried_vga_string in output:
-    try:
-        os.system("xrandr --output VGA1 --mode 1920x1080")
-        os.system("xrandr --output LVDS1 --off")
-    except Exception as e:
-        print("Error occour: {}".format(e))
-elif requried_vga_string not in output:
-    try:
-        #print("HIT")
-        os.system("xrandr --output LVDS1 --primary --mode 1024x600")
-        os.system("xrandr --output VGA1 --off")
-    except Exception as e:
-        print("Error occour: {}".format(e))
+    # Check if VGA cable is connected or not
+    # if connected then make VGA as primary and turn off laptop monitor
+    if requried_vga_string in output:
+        try:
+            os.system("xrandr --output VGA1 --mode 1920x1080")
+            os.system("xrandr --output LVDS1 --off")
+        except Exception as e:
+            print("Error occour: {}".format(e))
+    elif requried_vga_string not in output:
+        try:
+            #print("HIT")
+            os.system("xrandr --output LVDS1 --primary --mode 1024x600")
+            os.system("xrandr --output VGA1 --off")
+        except Exception as e:
+            print("Error occour: {}".format(e))
+
+    time.sleep(2)
 
 
